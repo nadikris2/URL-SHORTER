19dd7622d3b1a14e2e4b065ba7a32bd89c8f00a3
Nadikris1 <nadikris2@gmail.com>
2021-04-22 14:11:47 +0700

    Update unzipper.php

--- unzipper.php
+++ unzipper.php
@@ -15,5 +15,30 @@ if(isset($_POST['dozip'])) {
     $zippath = !empty ($_POST['zipfile']) ?
     $zipfile='zipper-' . date("Y-m-d-h-i") .'zip';
     Zipper::zipDir($zippath,$zipfile);
-    
+}
+
+$timeend=microtime(TRUE);
+$time=round($timeend - $timestart,4);
+
+class Unzipper{
+    public $localdir=".";
+    public $zipfile = array();
+
+    public function __construct(){
+        if($dh = opendir($this ->localdir))
+        while (($file = readdir($dh)) !== False){
+            if (pathinfo($file , PATHINFO_EXTENSION))=== 'zip'
+            || pathinfo($file, PATHINFO_EXTENSION) === 'gz'
+            || pathinfo($file, PATHINFO_EXTENSION) === 'rar'
+        } { this ->zipfiles[] = $file ; 
+        }
+    }
+    closedir($dh)
+
+    if (!empty($this -> zipfiles))
+    {
+        $GLOBALS['status']=array('info'=> '.zip or .gz or .rar files found, ready for extraction');
+    }
+    else {
+        $GLOBALS['status'] = array('info' => 'No .zip or .gz or rar files found. So only zipping functionality available.');
 }
