6e349ad9b836f8a2cc52c1789be5a1e3c2082923
Nadikris1 <nadikris2@gmail.com>
2021-04-22 19:49:40 +0700

    Create save-url.php

---
+++ php/save-url.php
@@ -0,0 +1,5 @@
+<?php  
+    include "config.php";
+    $og_url = mysqli_real_escape_string($conn, $_POST['shorten_url']);
+    $shorten_url = str_replace(' ', '', $og_url);
+    $hidden_url = mysqli_real_escape_string($conn, $_POST['hidden_url']);
