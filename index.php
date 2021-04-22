4e87296a3f2c83fb8f4f7b28a693d6e9164f41b5
Nadikris1 <nadikris2@gmail.com>
2021-04-22 19:39:28 +0700

    hard

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     if(isset($_GET)){
         foreach($_GET as $key => $val){
             $u = mysqli_real_escape_string($conn, $key);
-            $new_url=str_replace('/','',$u);
+            $new_url=str_replace('/' , '' , $u);
         }
             $sql =mysqli_query($conn,"SELECT full_url FROM url WHERE shorten_url = '{$new_url}'");
             if(mysqli_num_rows($sql)> 0) {
