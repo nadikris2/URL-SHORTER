8c3ff9cba18010e52821535162642b231ecc88ed
Nadikris1 <nadikris2@gmail.com>
2021-04-23 12:24:04 +0700

    Update unzipper.php

--- unzipper.php
+++ unzipper.php
@@ -146,3 +146,92 @@ class Unzipper{
           }
         }
         ?>
+        <!DOCTYPE html>
+<html>
+<head>
+  <title>File Unzipper + Zipper</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <style type="text/css">
+    <!--
+    body {
+      font-family: Arial, sans-serif;
+      line-height: 150%;
+    }
+
+    label {
+      display: block;
+      margin-top: 20px;
+    }
+
+    fieldset {
+      border: 0;
+      background-color: #EEE;
+      margin: 10px 0 10px 0;
+    }
+
+    .select {
+      padding: 5px;
+      font-size: 110%;
+    }
+
+    .status {
+      margin: 0;
+      margin-bottom: 20px;
+      padding: 10px;
+      font-size: 80%;
+      background: #EEE;
+      border: 1px dotted #DDD;
+    }
+
+    .status--ERROR {
+      background-color: red;
+      color: white;
+      font-size: 120%;
+    }
+
+    .status--SUCCESS {
+      background-color: green;
+      font-weight: bold;
+      color: white;
+      font-size: 120%
+    }
+
+    .small {
+      font-size: 0.7rem;
+      font-weight: normal;
+    }
+
+    .version {
+      font-size: 80%;
+    }
+
+    .form-field {
+      border: 1px solid #AAA;
+      padding: 8px;
+      width: 280px;
+    }
+
+    .info {
+      margin-top: 0;
+      font-size: 80%;
+      color: #777;
+    }
+
+    .submit {
+      background-color: #378de5;
+      border: 0;
+      color: #ffffff;
+      font-size: 15px;
+      padding: 10px 24px;
+      margin: 20px 0 20px 0;
+      text-decoration: none;
+    }
+
+    .submit:hover {
+      background-color: #2c6db2;
+      cursor: pointer;
+    }
+    -->
+  </style>
+</head>
+<body>
